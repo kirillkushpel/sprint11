0039384c00f3f4e6cb22b98e8b5ea384440b8b44
Kirill Kushpel <kirillkushpel@gmail.com>
2019-08-31 16:10:54 +0300

    webpack fix

--- src/script/options.js
+++ src/script/options.js
@@ -1,5 +1,5 @@
 export const options = {
-    host: 'http://praktikum.tk',
+    host: 'https://praktikum.tk',
     cohort: 'cohort1',
     token: 'b0f7914b-0b67-4fc8-95a9-59a1c36c47f7',
     contentType: `application/json`
